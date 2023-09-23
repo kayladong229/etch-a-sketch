75714a60fa8f08b44a9f0741558a930560beedaa
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2023-09-22 17:14:59 -0700

    Added hover functionality for color change

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ gridWidth = 960;
 function createDiv(num) {
     for (let i = 1; i <= num ** 2; i++) {
         const squareDiv = document.createElement('div');
+        squareDiv.setAttribute('id', 'square');
         squareDiv.style.border = '1px solid black';
         squareDiv.style.margin = '0px';
         squareDiv.style.height = ((gridHeight/num) - 2) + 'px'; //subtract 2 due to border
         squareDiv.style.width = ((gridWidth/num) - 2) + 'px';
 
         grid.appendChild(squareDiv);
+
+        squareDiv.addEventListener('mouseover', () => {
+            squareDiv.style.backgroundColor = 'aqua';
+        })
     }
 }
 
